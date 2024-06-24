73c0fe3d29729e47432f18273c390fa5654a1c13
Mathan Kumar <167211188+mathankumar250310@users.noreply.github.com>
2024-06-24 14:24:03 +0530

    Create addlast.java

---
+++ addlast.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+class addlast{
+    public static void main(String[]args){
+        LinkedList<String> name =new LinkedList<>();
+        name.add("HARIPRASATH");
+        name.add("ARI");
+        name.add("MATHAN");
+        name.add("BALA");
+        name.add("GOWTHAM");
+        name.addLast("ADHI");
+        System.out.println(name);
+
+    }
+}
