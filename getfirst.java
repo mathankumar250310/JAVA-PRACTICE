27ab708a5ff6b89c01603fe6ed8c553810ff16da
Mathan Kumar <167211188+mathankumar250310@users.noreply.github.com>
2024-06-24 14:37:18 +0530

    Create getfirst.java

---
+++ getfirst.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+class getfirst{
+    public static void main(String[]args){
+        LinkedList<String> name =new LinkedList<>();
+        name.add("HARIPRASATH");
+        name.add("ARI");
+        name.add("MATHAN");
+        name.add("BALA");
+        name.add("GOWTHAM");
+        name.getFirst();
+        System.out.println(name.getFirst());
+
+    }
+}
