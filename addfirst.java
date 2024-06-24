b552e02490b4e57fbd4a0f41b1b355063509b906
Mathan Kumar <167211188+mathankumar250310@users.noreply.github.com>
2024-06-24 14:14:37 +0530

    Create addfirst.java

---
+++ addfirst.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+class addfirst{
+    public static void main(String[]args){
+        LinkedList<String> name =new LinkedList<>();
+        name.add("HARIPRASATH");
+        name.add("ARI");
+        name.add("MATHAN");
+        name.add("BALA");
+        name.add("GOWTHAM");
+        name.addFirst("ADHI");
+        System.out.println(name);
+
+    }
+}
