9bef68d14854cad328881fe12280859a92c2e726
Mathan Kumar <167211188+mathankumar250310@users.noreply.github.com>
2024-06-25 15:13:05 +0530

    Create reverselinkedlist.java

---
+++ reverselinkedlist.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.LinkedList;
+class reversell{
+    public static void main(String[]args){
+        LinkedList<String> number =new LinkedList<>();
+        number.add("10");
+        number.add("20");
+        number.add("30");
+        number.add("40");
+        number.add("50");
+       
+        System.out.println(number.reversed());
+      
+}
+}
