57716bd9287b15fa1ef6af8b1b649dd70a75cbda
Mathan Kumar <167211188+mathankumar250310@users.noreply.github.com>
2024-06-25 14:21:21 +0530

    Create ifcondtionlinkedlist.java

---
+++ ifcondtionlinkedlist.java
@@ -0,0 +1,18 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+import java.util.LinkedList;
+
+public class ifconditionbyusinglinkedlist {
+   public ifconditionbyusinglinkedlist() {
+   }
+
+   public static void main(String[] args) {
+      LinkedList technology = new LinkedList();
+      technology.add("c,c++");
+      technology.add("React");
+      technology.add("SQL");
+      technology.add("PHP");
+      technology.add("Java");
+      technology.add("Angular");
+      System.out.println(technology);
+   }
+}
